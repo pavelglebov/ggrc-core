581abb26c01683742f488c8fc10f79d17c442b60
pavelglebov <pg.incloud@gmail.com>
2018-09-04 12:56:42 +0300

    Add simple-modal dismiss handler to people-group
    Fix issue on info-pin, when See More clicked and closed
    in editable-people-group section, people list was not updated

--- src/ggrc-client/js/components/people/editable-people-group.js
+++ src/ggrc-client/js/components/people/editable-people-group.js
@@ -112,5 +112,8 @@ export default can.Component.extend({
         viewModel.save();
       }
     },
+    '{window} simple-modal:dismiss': function (el, event) {
+      this.viewModel.attr('editableMode', false);
+    },
   },
 });
